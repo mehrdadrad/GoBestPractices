619b612739ff8dff0eab13d102e262c41a391383
Mehrdad Arshad Rad <mehrdad@edgecast.com>
2016-06-08 20:54:09 +0000

    init sync pool example

---
+++ syncpool.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Contact struct {
+	Name string
+	Age  uint
+}
+
+func main() {
+	var contactPool = sync.Pool{
+		New: func() interface{} {
+			return &Contact{}
+		},
+	}
+
+	c := contactPool.Get().(*Contact)
+	defer contactPool.Put(c)
+
+	c.Name = "Mehrdad"
+	c.Age = 40
+
+	fmt.Println(c.Name)
+}
