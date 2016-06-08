b929a2d6afa290cf3cbe5abb45db8f750286ef31
Mehrdad Arshad Rad <mehrdad@edgecast.com>
2016-06-08 22:59:27 +0000

    fixed go func

--- clang.go
+++ clang.go
@@ -1,20 +1,20 @@
 package main
 
 /*
-extern void print(int i);
+extern void Print(int i);
 
 void foo(void) {
 	int i;
 	for (i=0;i<10;i++) {
-		myprintf(i);
+		Print(i);
 	}
 }
 */
 import "C"
 import "fmt"
 
-//export print
-func print(i C.int) {
+//export Print
+func Print(i C.int) {
 	fmt.Println(uint32(i))
 }
 
